1b6fe7c31f827fbf6ea4a8be4ac420b150e15259
Ruona Dibie <ogheneruona.dibie@gmail.com>
2021-10-07 17:18:34 -0700

    Added a returnTo to remember previous page

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be logged in");
+    req.session.returnTo = req.originalUrl;
     return res.redirect("/login");
   }
 
