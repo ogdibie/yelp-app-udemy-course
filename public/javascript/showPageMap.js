f56fbe5a3cb9e2bc7e9bd36e009d82e60997ba57
Ruona Dibie <ogheneruona.dibie@gmail.com>
2021-10-14 11:25:17 -0700

    Added controls to map and styles

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -6,7 +6,7 @@ const map = new mapboxgl.Map({
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
-
+map.addControl(new mapboxgl.NavigationControl());
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
